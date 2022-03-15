ebf7db65b5ba0b6e7be9347559e3246233dbcaee
giorgio <giorgiolink94@hotmail.it>
2022-03-15 15:45:40 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import CustomNavbar from './components/NavBar';
+import Customfooter from './components/footer';
+import CustomJumbo from './components/Welcome';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    <>
+      <CustomNavbar/>
+      <Customfooter/>
+      <CustomJumbo/>
+    </>
+    
+    );
 }
 
 export default App;
---
+++ src/components/footer.jsx
@@ -0,0 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Card, Button } from 'react-bootstrap'
+
+
+
+const Customfooter = () => {
+    return (
+
+        <Card className='fixed-bottom'>
+  <Card.Header>Featured</Card.Header>
+  <Card.Body>
+    <Card.Title>Special title treatment</Card.Title>
+    <Card.Text>
+      With supporting text below as a natural lead-in to additional content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+    )
+}
+
+export default Customfooter
